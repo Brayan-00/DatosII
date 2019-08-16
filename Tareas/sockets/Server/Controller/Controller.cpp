04b982b70a83eccfaf64d2303caadcbd37b7224e
Brayan-00 <brayanfernando.10@gmail.com>
2019-08-16 15:52:28 -0600

    Se inicia el envío de Json mediante los sockets

---
+++ Tareas/sockets/Server/Controller/Controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brayanrv on 16/8/19.
+//
+
+#include "Controller.h"
---
+++ Tareas/sockets/Server/Controller/Controller.h
@@ -0,0 +1,14 @@
+//
+// Created by brayanrv on 16/8/19.
+//
+
+#ifndef TAREAS_CONTROLLER_H
+#define TAREAS_CONTROLLER_H
+
+
+class Controller {
+
+};
+
+
+#endif //TAREAS_CONTROLLER_H
