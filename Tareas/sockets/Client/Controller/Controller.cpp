13360a163d2a192ce23549922d33d0923f153aae
Brayan-00 <brayanfernando.10@gmail.com>
2019-08-16 00:10:49 -0600

    peq cambios

---
+++ Tareas/sockets/Client/Controller/Controller.cpp
@@ -0,0 +1,13 @@
+//
+// Created by brayanrv on 15/8/19.
+//
+
+#include "Controller.h"
+#include "include/rapidjson/rapidjson.h"
+#include "include/rapidjson/document.h"
+
+char message[];
+
+int main(){
+
+}
